f007b08f539316fcc6226905929dde54f58a6dca
Cesar Iparrea <cesariparrea@hotmail.com>
2024-03-28 10:37:52 -0400

    using args and kwargs

--- python_functions.py
+++ python_functions.py
@@ -53,3 +53,37 @@
 # print(dev_skills('Alex', 'HTML', 'CSS', 'JavaScript', 'Python'))
 
 
+
+# ____________________________________________________________________
+# KWARGS KewWordARGumentS breaks the arguments into a DICTIONARY(JS object) so now we have key value pairs instead of calling them with the index
+# KWARGS ONLY TAKES KEY VALUE PAIRS 
+
+# def dev_skills(dev_name, **kwargs):
+#   # kwargs will be a dictionary!
+#   dev = {'name': dev_name, 'skills': kwargs}
+#   return dev
+
+# print(dev_skills('Jackie', HTML=5, CSS=3, JavaScript=4, Python=2))
+
+
+
+# ____________________________________________________________________
+# KWARGS deconstruction to get keyword and value (line 79)
+# item is every key-value-pair inside a dictionary on this case it is called unpacking
+
+def arg_demo(pos1, pos2, *args, **kwargs):
+  print(f'Positional params: {pos1}, {pos2}')
+  print('*args:')
+  for arg in args:
+    print(' ', arg)
+  print('**kwargs:')
+
+# must be called like this, if not it will not recognize that you're calling both of the key-value-pairs
+  for keyword, value in kwargs.items():
+    print(f'  {keyword}: {value}')
+
+arg_demo('A', 'B', 1, 2, 3, color='purple', shape='circle')
+
+
+
+# ____________________________________________________________________
