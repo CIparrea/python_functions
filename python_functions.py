8bfb82d1ce7743a6e7525b8e718ea03d3d62f15d
Cesar Iparrea <cesariparrea@hotmail.com>
2024-03-28 10:19:42 -0400

    Breaking parameters using *args

---
+++ python_functions.py
@@ -0,0 +1,41 @@
+# ____________________________________________________________________
+# calling a function with some parameters
+
+# def add(a,b):
+#     return a+b
+
+# def sub(a, b):
+#   return a - b
+
+# def compute(a, b, op):
+#   return op(a, b)
+
+# print( compute(1, 5, sub) )
+
+
+
+
+# ____________________________________________________________________
+# printing all parameters passed to the function
+
+
+# def my_function(*args):
+#   print( type(args) )
+#   for arg in args:
+#     print(arg)
+
+# my_function(1, 2, 'SEI')
+
+
+
+
+# ____________________________________________________________________
+# breaking the parameters, the first argument is a name and then the rest will be considered as args
+
+def dev_skills(dev_name, *args):
+  dev = {'name': dev_name, 'skills': []}
+  for skill in args:
+    dev['skills'].append(skill)
+  return dev
+
+print(dev_skills('Alex', 'HTML', 'CSS', 'JavaScript', 'Python'))
